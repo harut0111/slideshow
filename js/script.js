7f45f62864634c2b36f8d070d585b5b3d99f6575
Harut Muqoyan <hmuqoyan@gmail.com>
2019-06-07 12:09:02 +0400

    added new images

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 
-let imgSrcArr = ['img/1.jpg', 'img/2.jpg', 'img/3.jpg', 'img/4.jpg', 'img/5.jpg', 'img/6.jpg', 'img/7.jpg']
+let imgSrcArr = [
+    'img/1.jpg', 'img/2.jpg', 'img/3.jpg', 'img/4.jpg', 'img/5.jpg', 
+    'img/6.jpg', 'img/7.jpg', 'img/8.jpg', 'img/9.jpg', 'img/10.jpg',
+    'img/11.jpg', 'img/12.jpg', 'img/13.jpg', 'img/14.jpg', 'img/15.jpg', 'img/16.jpg'
+]
 
 let buttons = document.getElementsByClassName('button');
 let image = document.getElementsByTagName('IMG')[0];
