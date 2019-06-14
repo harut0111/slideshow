1cf0060591576aaa622b63c771f6478d5c3091df
Harut Muqoyan <hmuqoyan@gmail.com>
2019-06-14 21:13:00 +0400

    completed

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 let imgSrcArr = [
     'img/1.jpg', 'img/2.jpg', 'img/3.jpg', 'img/4.jpg', 'img/5.jpg', 
     'img/6.jpg', 'img/7.jpg', 'img/8.jpg', 'img/9.jpg', 'img/10.jpg',
-    'img/11.jpg', 'img/12.jpg', 'img/13.jpg', 'img/14.jpg', 'img/15.jpg', 'img/16.jpg'
+    'img/11.jpg', 'img/12.jpg', 'img/13.jpg'
 ]
 
 let buttons = document.getElementsByClassName('button');
