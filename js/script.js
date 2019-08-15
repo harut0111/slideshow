cf996675deeea90d6866ef769f8e570c4933f173
Harut Muqoyan <hmuqoyan@gmail.com>
2019-08-15 13:49:01 +0400

    added random images

--- js/script.js
+++ js/script.js
@@ -1,40 +1,57 @@
-
-let imgSrcArr = [
-    'img/1.jpg', 'img/2.jpg', 'img/3.jpg', 'img/4.jpg', 'img/5.jpg', 
-    'img/6.jpg', 'img/7.jpg', 'img/8.jpg', 'img/9.jpg', 'img/10.jpg',
-    'img/11.jpg', 'img/12.jpg', 'img/13.jpg'
-]
+//  api images random /google
+//  https://picsum.photos/
+//  https://dog.ceo/dog-api/
 
 let buttons = document.getElementsByClassName('button');
 let image = document.getElementsByTagName('IMG')[0];
 
-let prev = buttons[0], play = buttons[1], next = buttons[2]
+let prev = buttons[0], play = buttons[1], next = buttons[2];
 
 prev.addEventListener('click', subtract);
 play.addEventListener('click', slide);
 play.addEventListener('click', () => { play.classList.toggle('active') });
 next.addEventListener('click', add);
 
-let i = 0;
-function add() {
-    i++;
-    if (i === imgSrcArr.length) { i = 0 }
-    image.src = imgSrcArr[i]
-}
-
-function subtract() {
-    i--;
-    if (i === -1) { i = imgSrcArr.length - 1 }
-    image.src = imgSrcArr[i]
-}
-
 let id, bool = true;
 function slide() {
     if (bool) {
-        id = setInterval(add, 1000);
+        add();
+        id = setInterval(add, 2000);
         bool = false;
     } else {
         clearInterval(id);
         bool = true;
+    }
+}
+
+let imgId = 500;
+
+async function subtract() {
+    // let url = 'https://dog.ceo/api/breeds/image/random';
+    imgId--;
+    if(imgId >= 1 && imgId <= 1000) {
+        let url = `https://picsum.photos/id/${imgId}/800`;
+        let response = await fetch(url);
+    
+        if(response.ok) {
+            image.src = response.url;
+        } else {
+            subtract();
+        }
+    }
+}
+
+async function add() {
+    // let url = 'https://dog.ceo/api/breeds/image/random';
+    imgId++;
+    if(imgId >= 1 && imgId <= 1000) {
+        let url = `https://picsum.photos/id/${imgId}/800`;
+        let response = await fetch(url);
+    
+        if(response.ok) {
+            image.src = response.url;
+        } else {
+            add();
+        }
     }
 }
